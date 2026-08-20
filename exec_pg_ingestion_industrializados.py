e5515078c9a0d6c1160b73c82c026e97672200c5
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2026-08-20 12:48:39 -0300

    feat: Formatando saída no terminal

--- exec_pg_ingestion_industrializados.py
+++ exec_pg_ingestion_industrializados.py
@@ -30,7 +30,7 @@ for key, val in os.environ.items():
         con_pg_script = con_pg_script.replace(target, val)
 
 # Loop na lista de arquivos
-for arquivo in arquivos[:2]:
+for arquivo in arquivos:
     print(50*'-')
     print(f'Arquivo: {arquivo.split('/')[-1]}')
     abs_path = f's3://dir-dados-abertos/{arquivo}'
@@ -38,40 +38,49 @@ for arquivo in arquivos[:2]:
     # Preenche a query de transferêcia de dados com o caminho do arquivo em processamento
     s3_to_pg_script_replaced = s3_to_pg_script.replace('{arquivo}', abs_path)
     #print(s3_to_pg_script_replaced)
-    inicio = datetime.now()
-    print(f'Início: {inicio.strftime('%H:%M')}')
 
     try:
-        # Verifica se a tebela já possui linhas do arquivo em processamento
         print('Abrindo conexão com o duckdb...')
         con = duckdb.connect()
         print('Abrindo conexão com o postgres...')
+
+        # Verifica se a tabela já possui linhas do arquivo em processamento
         verify_rows = con.execute(f"""
             {con_pg_script}
-            SELECT COUNT(filename)
-            FROM pg.industrializados
-            WHERE filename = '{abs_path}'
-        """).fetchone()
+            SELECT EXISTS (
+                SELECT 1 
+                FROM pg.industrializados 
+                WHERE filename = '{abs_path}'
+            );
+        """).fetchone()[0]
         # Se a tabela já possuir dados do arquivo em processamento, pula para o proximo arquivo
-        if verify_rows[0]:
+        if verify_rows:
             print('Já existem dados deste arquivo na tabela\nCarregando o próximo arquivo...')
+            print('Fechando conexão com o postgres...')
             con.execute("DETACH pg;")
             continue
+
         print('Transferindo dados...')
+        inicio = datetime.now()
+        print(f'Início: {inicio.strftime('%H:%M')}')
+
         con.execute(s3_to_pg_script_replaced)
         print('Dados transferidos com sucesso!')
-        con.execute("DETACH pg;")
+
+        fim = datetime.now()
+        print(f'Fim: {fim.strftime('%H:%M')}')
+
+        tempo = fim - inicio
+        print(f'Tempo da transferência: {tempo}')
+
         print('Fechando conexão com o postgres...')
+        con.execute("DETACH pg;")
     except Exception as e:
         print(f"Problema com a conexão: {e}")
+        print('Fechando conexão com o postgres...')
         con.execute("DETACH pg;")
         break
     finally:
         print('Fechando conexão com o duckdb...')
         con.close()
 
-    fim = datetime.now()
-    print(f'Fim: {fim.strftime('%H:%M')}')
-
-    tempo = fim - inicio
-    print(f'Tempo da transferência: {tempo}')
