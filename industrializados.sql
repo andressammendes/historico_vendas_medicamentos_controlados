879cafae3fce322aee0940ae510b5b1232a757d2
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2026-08-31 12:19:22 -0300

    refactor: carregamento agregado e filtrado por ano

--- industrializados.sql
+++ industrializados.sql
@@ -6,7 +6,8 @@ SELECT
     municipio,
     principio_ativo,
     qt_vendida,
-    conselho_prescritor,
+    NO_CONSELHO_PRESCRITOR AS conselho_prescritor,
+    -- conselho_prescritor,
     cid,
     filename
 FROM read_parquet(
--- manipulados.sql
+++ manipulados.sql
@@ -1,18 +1,40 @@
-CREATE OR REPLACE TABLE pg.manipulados AS 
+CREATE OR REPLACE TABLE pg.manipulados_novo AS 
+WITH tb_filtro AS (
+    SELECT
+        ano,
+        mes,
+        uf,
+        municipio,
+        NO_PRINCIPIO_ATIVO AS principio_ativo,
+        qt_ativo_por_unidade_farmacotec,
+        qtd_unidade_farmacotecnica,
+        NO_CONSELHO_PRESCRITOR AS conselho_prescritor,
+        cid
+    FROM read_parquet(
+        's3://dir-dados-abertos/bronze/Manipulados/**/*.parquet',
+        union_by_name = true,
+        filename = true
+    )
+    WHERE ano NOT IN [2014, 2015, 2016, 2026]
+)
+
 SELECT 
+    CAST(ano AS VARCHAR) || '/' || LPAD(CAST(mes AS VARCHAR), 2, '0') AS ano_mes,
+    uf,
+    municipio,
+    principio_ativo,
+    COUNT(principio_ativo) AS qt_vendida,
+    SUM(qt_ativo_por_unidade_farmacotec) AS qt_ativo_por_unidade_farmacotec,
+    SUM(qtd_unidade_farmacotecnica) AS qt_unidade_farmacotecnica,
+    conselho_prescritor,
+    cid
+FROM tb_filtro
+GROUP BY
     ano,
     mes,
     uf,
     municipio,
-    NO_PRINCIPIO_ATIVO as principio_ativo,
-    qt_ativo_por_unidade_farmacotec,
-    qtd_unidade_farmacotecnica,
-    NO_CONSELHO_PRESCRITOR as conselho_prescritor,
-    cid,
-    filename
-FROM read_parquet(
-    's3://dir-dados-abertos/bronze/Manipulados/**/*.parquet',
-    union_by_name = true,
-    filename = true
-)
-WHERE filename NOT LIKE '%2026%';
+    principio_ativo,
+    conselho_prescritor,
+    cid
+ORDER BY ano, mes;
